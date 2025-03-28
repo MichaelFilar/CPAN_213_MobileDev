ad85aa4a15b9c62272115a10e095cc3d14cd6794
Michael Filar <michaelfilar@hotmail.com>
2025-03-27 22:31:26 -0400

    buncha stuff done

--- assignments/filar_assignment3/App.js
+++ assignments/filar_assignment3/App.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'lightblue',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   titleStyle: {
     fontSize: 32,
